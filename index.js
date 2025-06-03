03a0b5c0bcdadb6beffd6a488e1a00f08407a2cb
elotrodiegoluna <elotrodiegoluna@gmail.com>
2025-06-03 17:33:21 -0400

    revertie

--- index.js
+++ index.js
@@ -8,7 +8,23 @@ import socketManager from './services/socketManager.js';
 const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 
+// crear instancia de IO
+const io = new Server(server, {
+    cors: {
+        origin: ['http://localhost:5175', 'https://gestion.altimec.cl'],
+        credentials: true
+    }
+});
+// inicializar servicio socketManager
+socketManager.initialize(io);
+
+io.on('connection', (socket) => {
+    socketManager.handleConnection(socket);
+});
+
 // iniciar servidor
 server.listen(PORT, () => {
   console.log(`ALTI.GESTION iniciado en el puerto ${PORT}`);
 });
+
+export { io };
