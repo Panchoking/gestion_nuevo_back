936a1c2729518463d5da4ce61ee7a0abd0cc09f2
elotrodiegoluna <elotrodiegoluna@gmail.com>
2025-06-03 17:23:32 -0400

    eliminar socket io

--- index.js
+++ index.js
@@ -8,20 +8,6 @@ import socketManager from './services/socketManager.js';
 const PORT = process.env.PORT || 3001;
 const server = http.createServer(app);
 
-// crear instancia de IO
-const io = new Server(server, {
-    cors: {
-        origin: ['http://localhost:5175', 'https://gestion.altimec.cl'],
-        credentials: true
-    }
-});
-// inicializar servicio socketManager
-socketManager.initialize(io);
-
-io.on('connection', (socket) => {
-    socketManager.handleConnection(socket);
-});
-
 // iniciar servidor
 server.listen(PORT, () => {
   console.log(`ALTI.GESTION iniciado en el puerto ${PORT}`);
